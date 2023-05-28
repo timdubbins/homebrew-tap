338b2835c83a8478739f51ac1116348d11e3d59d
tidub <57918941+tidub@users.noreply.github.com>
2023-05-28 23:02:29 +0100

    bump to v0.1.1

--- Formula/tap.rb
+++ Formula/tap.rb
@@ -1,9 +1,9 @@
 class Tap < Formula
   desc "Audio player for the terminal, written in Rust."
   homepage "https://github.com/timdubbins/tap"
-  url "https://github.com/timdubbins/tap/releases/download/v0.1.0/tap.tar.gz"
-  sha256 "0e4356d37e84452d29fbf98eebe4e11c82dbbb107768e3950a3d55d5295906ca"
-  version "0.1.0"
+  url "https://github.com/timdubbins/tap/releases/download/v0.1.1/tap_v0.1.1-x86_64-apple-darwin.tar.gz"
+  sha256 "57893148dbc58042b208103d928ff9fd682d2f937103eba5c1760fa24c32d291"
+  version "0.1.1"
 
   def install
     bin.install "tap"
