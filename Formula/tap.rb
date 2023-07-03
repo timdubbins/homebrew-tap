27e20f8011709c413c7a3a871544a04c51f50d4a
tidub <57918941+tidub@users.noreply.github.com>
2023-07-03 22:59:57 +0100

    fix url

--- Formula/tap.rb
+++ Formula/tap.rb
@@ -1,7 +1,7 @@
 class Tap < Formula
   desc "Audio player for the terminal, written in Rust."
   homepage "https://github.com/timdubbins/tap"
-  url "https://github.com/timdubbins/tap/releases/download/v0.2.0/tap_v0.2.0-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/timdubbins/tap/releases/download/v0.2.1/tap_v0.2.1-x86_64-apple-darwin.tar.gz"
   sha256 "ccb09e26f7719a3021772afbc6cf4c98716adb8582eadbf888a7031516670a30"
   version "0.2.1"
 
