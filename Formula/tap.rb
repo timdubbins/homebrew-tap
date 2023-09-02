0fe73e7ed16c93ab40e31b179d368581a7b4705b
tidub <57918941+tidub@users.noreply.github.com>
2023-09-02 16:57:24 +0100

    fix url for v0.4.3

--- Formula/tap.rb
+++ Formula/tap.rb
@@ -1,7 +1,7 @@
 class Tap < Formula
   desc "A audio player for the terminal with fuzzy-finder."
   homepage "https://github.com/timdubbins/tap"
-  url "https://github.com/timdubbins/tap/releases/download/v0.4.2/tap_v0.4.2-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/timdubbins/tap/releases/download/v0.4.3/tap_v0.4.3-x86_64-apple-darwin.tar.gz"
   sha256 "a379d63b818469caee3d766417d74558da0b7c8ac30f20c5918ffbe46bab520b"
   version "0.4.3"
 
