0f6766f4b8997402c22c8d8a3a0ee02e10be4c9c
tidub <57918941+tidub@users.noreply.github.com>
2025-08-24 15:57:57 +0100

    update to v0.5.3

--- Formula/tap.rb
+++ Formula/tap.rb
@@ -1,9 +1,9 @@
 class Tap < Formula
   desc "An audio player for the terminal with fuzzy-finder."
   homepage "https://github.com/timdubbins/tap"
-  url "https://github.com/timdubbins/tap/releases/download/v0.5.2/tap_0.5.2-x86_64-apple-darwin.tar.gz"
-  sha256 "e8b2e1d86c7abf0fdae05cdafee7ae89d0c1a09a41f97fc7b2883b302eed6f5e"
-  version "0.5.2"
+  url "https://github.com/timdubbins/tap/releases/download/v0.5.3/tap_0.5.3-x86_64-apple-darwin.tar.gz"
+  sha256 "a894b58c5765bab2fddaaa38e6bc53f2d8f15e8288173cea8079e4d23311c1b9"
+  version "0.5.3"
 
   def install
     bin.install "tap"
